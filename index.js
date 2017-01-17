67d3ed2748bce38ce8219c48958e15f6c9afaa3e
Nate Gay <nate@ohnate.com>
2017-01-17 17:05:24 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 var express = require('express')
-var mongodb = require('mongodb')
-var MongoClient = mongodb.MongoClient
+var admin = require('firebase-admin')
+var validUrl = require('valid-url')
 
-var url = process.env.MongoDB_URI
+var serviceKey = process.env.SERVICE_KEY || require('./serviceKey.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceKey),
+  databaseURL: 'https://url-shortener-microservi-685f2.firebaseio.com'
+})
 
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
-
+var db = admin.database()
 var app = express()
+
+var checkValidURL = (data, res) => {
+  if (validUrl.isUri(data)){
+      var key = db.ref().push(data).key
+      return res.json({
+        url: data,
+        short: 'https://rocky-stream-76935.herokuapp.com/' + key
+      })
+  } else {
+    return checkValidShort(data, res)
+  }
+}
+
+var checkValidShort = (data, res) => {
+  var notValid = ['.', '#', '$', '[', ']']
+  var valid = true
+  notValid.forEach((item) => {
+    if (data.indexOf(item) !== -1) {
+      valid = false
+    }
+  })
+  if (!valid) {
+    return res.json({error: 'Please enter a valid URL or shortcode.'})
+  }
+  return lookupShort(data, res)
+}
+
+var lookupShort = (data, res) => {
+  return db.ref(data).once('value').then((snap) => {
+    if (!snap.val()) {
+      return res.json({error: 'Please enter a valid URL or shortcode.'})
+    }
+    return res.redirect(snap.val())
+  })
+}
+
+app.get('/*', (req, res) => {
+  var data = req.params[0] || null
+  if (!data) {
+    return res.end('Enter a valid URL or shortcode behind the trailing /')
+  }
+  return checkValidURL(data, res)
+})
 
 app.listen(process.env.PORT || 5000)
